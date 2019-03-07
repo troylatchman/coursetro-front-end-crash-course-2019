52a3db2c2876679ee77f866816a4476490942b5b
Troy Latchman <troylatchman@gmail.com>
2019-03-07 15:26:47 -0500

    add image bundling

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,22 @@ module.exports = {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(html)$/,
+        use: ['html-loader'],
+      },
     ],
   },
 };
