1eb50abb36c25107cfc6609e3fb918621c944ef0
Troy Latchman <troylatchman@gmail.com>
2019-03-07 22:02:18 -0500

    enable nav burger

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import './css/main.css';
 console.log('test');
+
+const menu = document.getElementById('menu');
+const nav = document.getElementById('nav');
+const exit = document.getElementById('exit');
+
+menu.addEventListener('click', e => {
+  nav.classList.toggle('hide-mobile');
+  e.preventDefault();
+});
+
+exit.addEventListener('click', e => {
+  nav.classList.add('hide-mobile');
+  e.preventDefault();
+});
